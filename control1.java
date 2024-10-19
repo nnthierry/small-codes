2a2fcbe0a8141ec98a1a8bef6ae41ad119ecbdb7
nnthierry <168302264+nnthierry@users.noreply.github.com>
2024-10-19 12:41:41 +0300

    Update control1.java

--- control1.java
+++ control1.java
@@ -1,9 +1,8 @@
-//Write a Java program to get a number from the user and print whether it is positive or negative
+//a Java program to get a number from the user and print whether it is positive or negative
 import java.util.Scanner;
 public class Exercise1 {
 
-    
-  public static void main(String[] args)
+public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         System.out.print("Input number: ");
